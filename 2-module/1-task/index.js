459fe41d16ec06ffc1f6e77b0d7dea55a9fadd2c
timosha <tinaatimkovaa@gmail.com>
2024-01-29 02:03:13 -0500

    solutions for module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let prop in salaries) {
+    if (isFinite(salaries[prop])) {
+      sum += salaries[prop];
+    }
+  }
+  return sum;
 }
