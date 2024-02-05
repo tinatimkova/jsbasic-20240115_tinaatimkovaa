fe12c8f41ebf6824a049d12ce06ecc467eb40e59
timosha <tinaatimkovaa@gmail.com>
2024-02-05 16:23:10 -0500

    task 3 is in progress

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,12 @@
 function highlight(table) {
-  // ваш код...
+  for (let row of table.querySelector('tbody').rows) {
+    let status = row.lastElementChild;
+    if (!status.hasAttribute('data-available')){
+      status.setAttribute('hidden', true);
+    } else if (status.dataset.available == 'true') {
+      status.className = 'available';
+    } else {
+      status.className = 'unavailable';
+    }
+  }
 }
