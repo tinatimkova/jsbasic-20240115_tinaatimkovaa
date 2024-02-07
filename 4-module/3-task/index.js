e0d8cbf6c972bc90c2bfc41803759b63e9c017e0
timosha <tinaatimkovaa@gmail.com>
2024-02-07 02:09:27 -0500

    solutions for module 4

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,12 +1,39 @@
 function highlight(table) {
-  for (let row of table.querySelector('tbody').rows) {
-    let status = row.lastElementChild;
-    if (!status.hasAttribute('data-available')){
-      status.setAttribute('hidden', true);
-    } else if (status.dataset.available == 'true') {
-      status.className = 'available';
-    } else {
-      status.className = 'unavailable';
+  let indexStatus, indexAge, indexGender;
+
+  let thead = table.tHead.querySelectorAll('td');
+
+  for (let td of thead) {
+    switch (td.innerHTML) {
+      case "Status": 
+        indexStatus = td.cellIndex;
+        break;
+      case "Gender":
+        indexGender = td.cellIndex;
+        break;
+      case "Age":
+        indexAge = td.cellIndex;
+        break;
     }
+  }
+  
+  let tbody = table.querySelector('tbody').rows;
+  
+  for (let tr of tbody) {
+
+    if (tr.cells[indexStatus].dataset.available) {
+
+     tr.cells[indexStatus].dataset.available === 'true'
+     ? tr.classList.add('available') 
+     : tr.classList.add('unavailable');
+     
+   } else {
+     tr.setAttribute('hidden', true);
+   }
+    
+    if (tr.cells[indexGender].innerHTML === 'm') tr.classList.add('male');
+    if (tr.cells[indexGender].innerHTML === 'f') tr.classList.add('female');
+    
+    if (tr.cells[indexAge].innerHTML < 18) tr.style.textDecoration = 'line-through';
   }
 }
