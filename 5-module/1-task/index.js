022eada4e7f4e5850ae42883c44c5121f1733535
timosha <tinaatimkovaa@gmail.com>
2024-02-12 17:10:37 -0500

    got carousel functionality. need to style arrows

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,5 @@
 function hideSelf() {
-  // ваш код...
+  let button = document.querySelector('.hide-self-button');
+  
+  button.onclick = () => button.hidden = true;
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,25 @@
 function initCarousel() {
-  // ваш код...
+
+  let carousel = document.querySelector('.carousel__inner');
+  let slides = document.querySelectorAll('.carousel__slide');
+
+  let slidesCount = slides.length - 1;
+  let index = 0;
+
+  let moveRight = function() { 
+    if (index < slidesCount) {
+      carousel.style.transform = `translateX(-${carousel.offsetWidth*(++index)}px)`;
+    }
+  } 
+    
+  let moveLeft = function() {
+    if (index > 0) { 
+      carousel.style.transform = `translateX(-${carousel.offsetWidth*(--index)}px)`;
+    }
+    console.log(index);
+  };
+
+  document.querySelector('.carousel__arrow_right').addEventListener('click', moveRight);
+  document.querySelector('.carousel__arrow_left').addEventListener('click', moveLeft);
 }
+
