eda74eff2213e1d3f2215123f428a655d38530b9
timosha <tinaatimkovaa@gmail.com>
2024-02-13 17:38:44 -0500

    solutions for module-5

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,24 +2,29 @@ function initCarousel() {
 
   let carousel = document.querySelector('.carousel__inner');
   let slides = document.querySelectorAll('.carousel__slide');
+  let arrowRight = document.querySelector('.carousel__arrow_right');
+  let arrowLeft = document.querySelector('.carousel__arrow_left');
 
   let slidesCount = slides.length - 1;
   let index = 0;
 
-  let moveRight = function() { 
-    if (index < slidesCount) {
-      carousel.style.transform = `translateX(-${carousel.offsetWidth*(++index)}px)`;
-    }
+  index == 0 && (arrowLeft.style.display = 'none');
+
+  let moveRight = function() {
+    if (index < slidesCount) carousel.style.transform = `translateX(-${carousel.offsetWidth*(++index)}px)`;
+
+    index != 0 && (arrowLeft.style.display = '');
+    index == slidesCount && (arrowRight.style.display = 'none');
   } 
     
   let moveLeft = function() {
-    if (index > 0) { 
-      carousel.style.transform = `translateX(-${carousel.offsetWidth*(--index)}px)`;
-    }
-    console.log(index);
+    if (index > 0) carousel.style.transform = `translateX(-${carousel.offsetWidth*(--index)}px)`;
+
+    index == 0 && (arrowLeft.style.display = 'none');
+    index !== slidesCount && (arrowRight.style.display = '');
   };
 
-  document.querySelector('.carousel__arrow_right').addEventListener('click', moveRight);
-  document.querySelector('.carousel__arrow_left').addEventListener('click', moveLeft);
+  arrowRight.addEventListener('click', moveRight);
+  arrowLeft.addEventListener('click', moveLeft);
 }
 
